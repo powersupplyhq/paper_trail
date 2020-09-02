22e984958b426a167f24ef9375878595c5bf5c55
Chris Hall <christopher.k.hall@gmail.com>
2020-09-02 13:17:17 -0400

    fix papertrail issue when running `rake db:create`

--- lib/paper_trail/model_config.rb
+++ lib/paper_trail/model_config.rb
@@ -148,6 +148,8 @@ module PaperTrail
       return unless options.key?(:limit)
       return if version_class.item_subtype_column_present?
       raise format(E_MODEL_LIMIT_REQUIRES_ITEM_SUBTYPE, @model_class.name)
+    rescue ActiveRecord::NoDatabaseError
+      ::Rails.logger.warn "Database does not Exist! - not loading papertrail"
     end
 
     def check_version_class_name(options)
